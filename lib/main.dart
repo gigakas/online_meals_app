bbcb34da01a56386f5146385f006db2eee1319f9
gigakas <guillermo@albanss.com>
2025-05-16 20:59:44 +0200

    base main

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,7 @@ import 'package:google_fonts/google_fonts.dart';
 
 final theme = ThemeData(
   useMaterial3: true,
-  colorScheme: ColorScheme.fromSeed(
-    brightness: Brightness.dark,
-    seedColor: const Color.fromARGB(255, 131, 57, 0),
-  ),
+  colorScheme: ColorScheme.fromSeed(brightness: Brightness.dark, seedColor: const Color.fromARGB(255, 131, 57, 0)), //
   textTheme: GoogleFonts.latoTextTheme(),
 );
 
@@ -20,9 +17,6 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: theme,
-      home: Scaffold(),
-    );
+    return MaterialApp(theme: theme, home: Scaffold());
   }
 }
